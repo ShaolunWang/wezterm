ac4aa000c202df3c0260e45a17ea346997519e9e
ShaolunWang <wang.shaolun@gmail.com>
2024-08-13 09:54:31 +0100

    stuff

--- theme.lua
+++ theme.lua
@@ -1,201 +1,300 @@
-local wezterm = require('wezterm')
+local wez = require('wezterm')
+
+local config = {
+	position = 'bottom',
+	max_width = 32,
+	left_separator = '  ',
+	right_separator = '  ',
+	field_separator = '  |  ',
+	leader_icon = '',
+	workspace_icon = '',
+	pane_icon = '',
+	user_icon = '',
+	hostname_icon = '󰒋',
+	clock_icon = '󰃰',
+	cwd_icon = '',
+	enabled_modules = {
+		username = true,
+		hostname = true,
+		clock = true,
+		cwd = true,
+	},
+	ansi_colors = {
+		workspace = 8,
+		leader = 2,
+		pane = 7,
+		active_tab = 4,
+		inactive_tab = 6,
+		username = 6,
+		hostname = 8,
+		clock = 5,
+		cwd = 7,
+	},
+}
+
+local username = os.getenv('USER') or os.getenv('LOGNAME') or os.getenv('USERNAME')
+local home = (os.getenv('USERPROFILE') or os.getenv('HOME') or wez.home_dir or ''):gsub('\\', '/')
+local is_windows = package.config:sub(1, 1) == '\\'
 
 local M = {}
 
-wezterm.on('update-right-status', function(window, pane)
-	local cols = pane:get_dimensions().cols
-	local user_vars = pane:get_user_vars()
+local function tableMerge(t1, t2)
+	for k, v in pairs(t2) do
+		if type(v) == 'table' then
+			if type(t1[k] or false) == 'table' then
+				tableMerge(t1[k] or {}, t2[k] or {})
+			else
+				t1[k] = v
+			end
+		else
+			t1[k] = v
+		end
+	end
+	return t1
+end
 
-	local icon = user_vars.tab_icon
-	if not icon or icon == '' then
-		-- fallback for the icon,
-		icon = ''
+local find_git_dir = function(directory)
+	directory = directory:gsub('~', home)
+
+	while directory do
+		local handle = io.open(directory .. '/.git/HEAD', 'r')
+		if handle then
+			handle:close()
+			directory = directory:match('([^/]+)$')
+			return directory
+		elseif directory == '/' or directory == '' then
+			break
+		else
+			directory = directory:match('(.+)/[^/]*')
+		end
 	end
 
-	local icon_color = user_vars.tab_color
-	if not icon_color then
-		icon_color = 'white'
+	return nil
+end
+
+local get_cwd_hostname = function(pane, search_git_root_instead)
+	local cwd, hostname = '', ''
+	local cwd_uri = pane:get_current_working_dir()
+	if cwd_uri then
+		if type(cwd_uri) == 'userdata' then
+			-- Running on a newer version of wezterm and we have
+			-- a URL object here, making this simple!
+
+			---@diagnostic disable-next-line: undefined-field
+			cwd = cwd_uri.file_path
+			---@diagnostic disable-next-line: undefined-field
+			hostname = cwd_uri.host or wez.hostname()
+		else
+			-- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+			-- which doesn't have the Url object
+			cwd_uri = cwd_uri:sub(8)
+			local slash = cwd_uri:find('/')
+			if slash then
+				hostname = cwd_uri:sub(1, slash - 1)
+				-- and extract the cwd from the uri, decoding %-encoding
+				cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
+					return string.char(tonumber(hex, 16))
+				end)
+			end
+		end
+
+		-- Remove the domain name portion of the hostname
+		local dot = hostname:find('[.]')
+		if dot then
+			hostname = hostname:sub(1, dot - 1)
+		end
+		if hostname == '' then
+			hostname = wez.hostname()
+		end
+
+		if is_windows then
+			cwd = cwd:gsub('/' .. home .. '(.-)$', '~%1')
+		else
+			cwd = cwd:gsub(home .. '(.-)$', '~%1')
+		end
+
+		---search for the git root of the project if specified
+		if search_git_root_instead then
+			local git_root = find_git_dir(cwd)
+			cwd = git_root or cwd ---fallback to cwd
+		end
 	end
 
-	window:set_left_status(wezterm.format({
-		{ Background = { Color = '#333333' } },
-		{ Foreground = { Color = icon_color } },
-		{ Text = ' ' .. wezterm.pad_right(icon, 3) },
-	}))
+	return cwd, hostname
+end
 
-	local title = pane:get_title()
-	local date = ' ' .. wezterm.strftime('%H:%M %d-%m-%Y') .. ' '
+local basename = function(path) -- get filename from path
+	if type(path) ~= 'string' then
+		return nil
+	end
+	local file = ''
+	if M.is_windows then
+		file = path:gsub('(.*[/\\])(.*)', '%2') -- replace (path/ or path\)(file) with (file)
+	else
+		file = path:gsub('(.*/)(.*)', '%2') -- replace (path/)(file) with (file)
+	end
+	-- remove extension
+	file = file:gsub('(%..+)$', '')
+	return file
+end
 
-	-- generate padding to center title by adding half of width (cols), half
-	-- of title length, length of date string and integrated buttons width
-	--
-	-- the 1 at the end is cause of extra space after date to separate it from
-	-- buttons
-	--
-	-- if there are any theming in date or title use `wezterm.column.width`
-	local padding = wezterm.pad_right('', (cols / 2) - (string.len(title) / 2) - string.len(date) - 3 * 3 - 1)
+local function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return basename(tab_info.active_pane.title)
+end
 
-	window:set_right_status(wezterm.format({
-		{ Text = ' ' .. title .. ' ' },
-		{ Background = { Color = '#333333' } },
-		{ Text = padding },
-		{ Background = { Color = '#444444' } },
-		{ Text = date },
-		{ Background = { Color = '#333333' } },
-		{ Text = ' ' },
-	}))
-end)
+local get_leader = function(prev)
+	local leader = config.leader_icon
 
-wezterm.on('format-tab-title', function(tab, _, _, _, _)
-	-- i could forget i've zoomed in and forget about a pane in a tab
-	local is_zoomed = ''
-	if tab.active_pane.is_zoomed then
-		is_zoomed = 'z'
+	wez.log_info('prev: ' .. prev)
+	wez.log_info('prev size: ' .. #prev)
+	wez.log_info('leader: ' .. leader)
+	wez.log_info('leader size: ' .. #leader)
+
+	local spacing = #prev - #leader
+	local first_half = math.floor(spacing / 2)
+	local second_half = math.ceil(spacing / 2)
+	wez.log_info('spacing: ' .. spacing)
+	wez.log_info('first_half: ' .. first_half)
+	wez.log_info('second_half: ' .. second_half)
+	return string.rep(' ', first_half) .. leader .. string.rep(' ', second_half)
+end
+
+-- conforming to https://github.com/wez/wezterm/commit/e4ae8a844d8feaa43e1de34c5cc8b4f07ce525dd
+-- exporting an apply_to_config function, even though we don't change the users config
+M.apply_to_config = function(c, opts)
+	-- make the opts arg optional
+	if not opts then
+		opts = {}
+	end
+
+	-- combine user config with defaults
+	config = tableMerge(config, opts)
+
+	local scheme = wez.color.get_builtin_schemes()[c.color_scheme]
+	local default_colors = {
+		tab_bar = {
+			background = 'transparent',
+			active_tab = {
+				bg_color = 'transparent',
+				--				fg_color = scheme.ansi[config.ansi_colors.active_tab],
+				fg_color = 'cyan',
+			},
+			inactive_tab = {
+				bg_color = 'transparent',
+				--				fg_color = scheme.ansi[config.ansi_colors.inactive_tab],
+
+				fg_color = 'grey',
+			},
+		},
+	}
+
+	if c.colors == nil then
+		c.colors = default_colors
+	else
+		c.colors = tableMerge(default_colors, c.colors)
+	end
+
+	c.use_fancy_tab_bar = false
+	--	c.tab_bar_at_bottom = config.position == 'bottom'
+	c.tab_max_width = config.max_width
+end
+
+wez.on('format-tab-title', function(tab, _, _, conf, _, _)
+	local palette = conf.resolved_palette
+
+	local index = tab.tab_index + 1
+	local offset = #tostring(index) + #config.left_separator + 2
+	local title = index .. config.left_separator .. tab_title(tab)
+
+	local width = conf.tab_max_width - offset
+	if #title > conf.tab_max_width then
+		title = wez.truncate_right(title, width) .. '…'
+	end
+
+	local fg = palette.tab_bar.inactive_tab.fg_color
+	local bg = palette.tab_bar.inactive_tab.bg_color
+	if tab.is_active then
+		fg = palette.tab_bar.active_tab.fg_color
+		bg = palette.tab_bar.active_tab.bg_color
 	end
 
 	return {
-		{ Text = tab.active_pane .. tab.tab_index + 1 .. is_zoomed .. ' ' },
+		{ Background = { Color = bg } },
+		{ Foreground = { Color = fg } },
+		{ Text = title .. '  ' },
 	}
 end)
 
-function M.apply(config)
-	--	config.color_scheme = 'TokyoNight'
-	--	config.color_scheme = 'Sea Shells'
-	config.font_size = 12
-	--config.color_scheme = 'Sea Shells (Gogh)'
-	--config.color_scheme = 'Ocean Dark (Gogh)'
-	-- config.color_scheme = 'Tender (Gogh)'
-	config.color_scheme = 'Tokyo Night'
+-- Name of workspace
+wez.on('update-status', function(window, pane)
+	local present, conf = pcall(window.effective_config, window)
+	if not present then
+		return
+	end
 
-	config.font = wezterm.font('BlexMono Nerd Font Mono')
+	local palette = conf.resolved_palette
 
-	config.window_padding = {
-		left = '6px',
-		right = '6px',
-		top = '2px',
-		bottom = 0,
+	-- left status
+	local stat = ' ' .. config.workspace_icon .. ' ' .. window:active_workspace() .. ' '
+	local stat_fg = palette.ansi[config.ansi_colors.workspace]
+
+	if window:leader_is_active() then
+		stat_fg = palette.ansi[config.ansi_colors.leader]
+		stat = get_leader(stat)
+	end
+
+	window:set_left_status(wez.format({
+		{ Background = { Color = palette.tab_bar.background } },
+		{ Foreground = { Color = stat_fg } },
+		{ Text = stat },
+
+		{ Foreground = { Color = palette.ansi[config.ansi_colors.pane] } },
+		{ Text = config.pane_icon .. ' ' .. basename(pane:get_title()) .. ' ' },
+	}))
+
+	-- right status
+	local cells = {
+		{ Background = { Color = palette.tab_bar.background } },
 	}
-	config.colors = {
-		tab_bar = {
-			-- The color of the strip that goes along the top of the window
-			-- (does not apply when fancy tab bar is in use)
-			background = '#0b0022',
+	local enabled_modules = config.enabled_modules
 
-			-- The active tab is the one that has focus in the window
-			active_tab = {
-				-- The color of the background area for the tab
-				bg_color = '#2b2042',
-				-- The color of the text for the tab
-				fg_color = '#c0c0c0',
+	if enabled_modules.username then
+		table.insert(cells, { Foreground = { Color = palette.ansi[config.ansi_colors.username] } })
+		table.insert(cells, { Text = username })
+		table.insert(cells, { Foreground = { Color = palette.brights[1] } })
+		table.insert(cells, { Text = config.right_separator .. config.user_icon .. config.field_separator })
+	end
 
-				-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-				-- label shown for this tab.
-				-- The default is "Normal"
-				intensity = 'Normal',
+	local cwd, hostname = get_cwd_hostname(pane, true)
+	if enabled_modules.hostname then
+		table.insert(cells, { Foreground = { Color = palette.ansi[config.ansi_colors.hostname] } })
+		table.insert(cells, { Text = hostname })
+		table.insert(cells, { Foreground = { Color = palette.brights[1] } })
+		table.insert(cells, { Text = config.right_separator .. config.hostname_icon .. config.field_separator })
+	end
 
-				-- Specify whether you want "None", "Single" or "Double" underline for
-				-- label shown for this tab.
-				-- The default is "None"
-				underline = 'None',
+	if enabled_modules.clock then
+		table.insert(cells, { Foreground = { Color = palette.ansi[config.ansi_colors.clock] } })
+		table.insert(cells, { Text = wez.time.now():format('%H:%M') })
+		table.insert(cells, { Foreground = { Color = palette.brights[1] } })
+		table.insert(cells, { Text = config.right_separator .. config.clock_icon .. '  ' })
+	end
 
-				-- Specify whether you want the text to be italic (true) or not (false)
-				-- for this tab.  The default is false.
-				italic = false,
+	if enabled_modules.cwd then
+		table.insert(cells, { Foreground = { Color = palette.brights[1] } })
+		table.insert(cells, { Text = config.cwd_icon .. ' ' })
+		table.insert(cells, { Foreground = { Color = palette.ansi[config.ansi_colors.cwd] } })
+		table.insert(cells, { Text = cwd .. ' ' })
+	end
 
-				-- Specify whether you want the text to be rendered with strikethrough (true)
-				-- or not for this tab.  The default is false.
-				strikethrough = false,
-			},
-
-			-- Inactive tabs are the tabs that do not have focus
-			inactive_tab = {
-				bg_color = '#1b1032',
-				fg_color = '#808080',
-
-				-- The same options that were listed under the `active_tab` section above
-				-- can also be used for `inactive_tab`.
-			},
-
-			-- You can configure some alternate styling when the mouse pointer
-			-- moves over inactive tabs
-			inactive_tab_hover = {
-				bg_color = '#3b3052',
-				fg_color = '#909090',
-				italic = true,
-
-				-- The same options that were listed under the `active_tab` section above
-				-- can also be used for `inactive_tab_hover`.
-			},
-
-			-- The new tab button that let you create new tabs
-			new_tab = {
-				bg_color = '#1b1032',
-				fg_color = '#808080',
-
-				-- The same options that were listed under the `active_tab` section above
-				-- can also be used for `new_tab`.
-			},
-
-			-- You can configure some alternate styling when the mouse pointer
-			-- moves over the new tab button
-			new_tab_hover = {
-				bg_color = '#3b3052',
-				fg_color = '#909090',
-				italic = true,
-
-				-- The same options that were listed under the `active_tab` section above
-				-- can also be used for `new_tab_hover`.
-			},
-		},
-	}
-	--[[ 	config.colors = {
-		tab_bar = {
-			background = '#333333',
-
-			active_tab = {
-				fg_color = '#ffffff',
-				bg_color = '#444444',
-			},
-
-			new_tab = {
-				bg_color = '#333333',
-				fg_color = '#ffffff',
-			},
-
-			new_tab_hover = {
-				bg_color = '#555555',
-				fg_color = '#ffffff',
-			},
-		},
-	} ]]
-
-	local window_min = ' 󰖰 '
-	local window_max = ' 󰖯 '
-	local window_close = ' 󰅖 '
-
-	config.window_decorations = 'INTEGRATED_BUTTONS | RESIZE'
-	config.integrated_title_buttons = { 'Hide', 'Maximize', 'Close' }
-	config.tab_bar_style = {
-		window_hide = window_min,
-		window_hide_hover = window_min,
-		window_maximize = window_max,
-		window_maximize_hover = window_max,
-		window_close = window_close,
-		window_close_hover = window_close,
-	}
-
-	-- makes the tabbar look more like TUI
-	config.use_fancy_tab_bar = false
-
-	config.window_frame = {
-		border_left_width = '4px',
-		border_right_width = '4px',
-		border_bottom_height = '4px',
-		border_left_color = config.colors.tab_bar.background,
-		border_right_color = config.colors.tab_bar.background,
-		border_bottom_color = config.colors.tab_bar.background,
-		border_top_color = config.colors.tab_bar.background,
-	}
-end
+	window:set_right_status(wez.format(cells))
+end)
 
 return M
--- wezterm.lua
+++ wezterm.lua
@@ -10,6 +10,16 @@ local wezterm = require('wezterm')
 --config.color_scheme = 'SeaShells'
 
 local config = {
+	window_decorations = 'INTEGRATED_BUTTONS | RESIZE',
+	integrated_title_buttons = { 'Hide', 'Maximize', 'Close' },
+	tab_bar_style = {
+		window_hide = window_min,
+		window_hide_hover = window_min,
+		window_maximize = window_max,
+		window_maximize_hover = window_max,
+		window_close = window_close,
+		window_close_hover = window_close,
+	},
 	audible_bell = 'Disabled',
 	check_for_updates = false,
 	inactive_pane_hsb = {
@@ -17,13 +27,18 @@ local config = {
 		saturation = 1.0,
 		brightness = 1.0,
 	},
+	font_size = 12,
+	font = wezterm.font('BlexMono Nerd Font Mono'),
+	--	color_scheme = 'tokyonight_night',
+	color_scheme = 'Monokai Pro Ristretto (Gogh)',
 	launch_menu = {},
 	leader = { key = ';', mods = 'CTRL' },
 	disable_default_key_bindings = true,
 	keys = require('key'),
 	set_environment_variables = {},
 	default_prog = { 'pwsh', '-NoLogo' },
---	default_prog = { 'elvish'},
+	--	default_prog = { 'elvish'},
 }
-require('theme').apply(config)
+require('theme').apply_to_config(config, { enabled_modules = { username = false, hostname = false } })
+
 return config
